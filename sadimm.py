f838a84e3dca1e5909915d54f8c6c228d352f5ba
princesadim <116694774+princesadim@users.noreply.github.com>
2022-10-26 14:01:30 +0600

    sadimm.py
    pkg install python
    pkg install python2
    pkg install git
    pip2 install requests
    pip2 install mechanize
    pip requests 
    Pip install mechanize
    rm -rf SADIMM
    git clone https://github.com/princesadim/sadimm.-py.git
    cd sadimm
    python sadimm.py

---
+++ sadimm.py
@@ -0,0 +1,34 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+        os.system('xdg-open https://github.com/princesadim')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from a import main
+ 
+        main()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from a import main
+ 
+ 
+        main()
+        
+ 
